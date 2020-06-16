bd3b5e574c4956a3e5fb5a7fac77f3bd918be309
Sahil Nare <sahilnare78@gmail.com>
2020-06-16 21:46:58 +0530

    Test commit

---
+++ src/client/pages/Upload.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logOut } from '../reduxStore/actions/authActions';
+import { Button } from '@zeit-ui/react';
+
+class Upload extends Component {
+
+  logOut = () => {
+    this.props.logOut();
+    // console.log(cred);
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <h1>Upload Files</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logOut: () => {
+      dispatch(logOut());
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Upload);
