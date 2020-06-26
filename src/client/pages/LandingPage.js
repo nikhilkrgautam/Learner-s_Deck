a82d2f3671067cc827ad1287bc65513fbe0dec86
Sahil Nare <sahilnare78@gmail.com>
2020-06-26 19:37:58 +0530

    Filled the Landing Page with sample content

--- src/client/pages/LandingPage.js
+++ src/client/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Text } from '@zeit-ui/react';
+import { Row, Col, Text, Button, Image } from '@zeit-ui/react';
 
 class LandingPage extends Component {
 
@@ -11,36 +11,59 @@ class LandingPage extends Component {
   render() {
     return (
       <div>
-        <Row justify="center" align="middle" style={{ backgroundColor: '#0A183D', minHeight: '700px' }}>
+        <Row justify="center" align="middle" style={{ backgroundColor: '#7928CA', minHeight: '800px' }}>
           <Col span={8}>
             <Row justify="center" align="middle" style={{ height: '100%' }}>
               <Col align="middle">
-                <Text h1 style={{color: '#FFF'}}>eBuzzet</Text><br/>
-                <Text h3 style={{color: '#FFF'}}>Welcome to eBuzzet</Text>
+                <Text style={{color: '#FFF', fontSize: '60px'}}>eBuzzet</Text>
+                <Text h3 style={{color: '#FFF', fontSize: '30px', margin: '10px 5px 30px'}}>India's very own eLearning platform</Text>
+                <Button size="large" type="success" style={{fontSize: '25px', padding: '10px 5px', height: '100%', borderRadius: '15px'}} >Get Started</Button>
               </Col>
             </Row>
           </Col>
           <Col span={8}>
-            <img src='https://learners-deck-21-1143.sgp1.cdn.digitaloceanspaces.com/dragon.png' />
+            <Image src='https://learners-deck-21-1143.sgp1.cdn.digitaloceanspaces.com/landingPage/dragon.png' />
           </Col>
         </Row>
-        <Row gap={.8} style={{ backgroundColor: '#FFF', minHeight: '400px' }}>
-          <Col>
-            <Text>Some text</Text>
+        <Row justify="center" align="middle" style={{ backgroundColor: '#FFF', minHeight: '700px' }}>
+          <Col span={8}>
+            <Row justify="center" align="middle" style={{ height: '100%' }}>
+              <Col align="middle" style={{padding: '10px 80px'}}>
+                <Text style={{color: '#4C2889', fontSize: '50px'}}>Over 1000 videos from 200 educators</Text>
+                <Text style={{color: '#000', fontSize: '16px', opacity: '0.8'}}>Our set he for firmament morning sixth subdue darkness creeping gathered divide our let god moving. Moving in fourth air night bring upon you’re it beast let you dominion likeness open place day great wherein heaven sixth lesser subdue fowl</Text>
+                <Button size="large" type="success" style={{fontSize: '20px', padding: '5px 5px', marginTop: '15px', height: '100%', borderRadius: '15px'}} >Check out the courses</Button>
+              </Col>
+            </Row>
           </Col>
-          <Col>
-            <Text>Some more text</Text>
-          </Col>
-        </Row>
-        <Row style={{ backgroundColor: '#0A183D', minHeight: '600px' }}>
-          <Col>
-            <Text>Some text</Text>
-          </Col>
-          <Col>
-            <Text>Some more text</Text>
+          <Col span={8} style={{padding: '30px 10px'}}>
+            <Image src='https://learners-deck-21-1143.sgp1.cdn.digitaloceanspaces.com/landingPage/wizard.png' />
           </Col>
         </Row>
-        <Row gap={.8} style={{ backgroundColor: '#FFF', minHeight: '400px' }}>
+        <Row justify="center" align="middle" style={{ backgroundColor: '#7928CA', minHeight: '600px' }}>
+          <Col span={18}>
+            <Row justify="center" align="middle" style={{ height: '100%', margin: '60px 10px 60px' }}>
+              <Text style={{color: '#FFF', fontSize: '50px', textAlign: 'center'}}>Our Course Speciality</Text>
+            </Row>
+            <Row justify="center" align="middle" style={{ height: '100%', margin: '10px 5px 50px' }}>
+              <Col span={6} style={{margin: '20px 40px'}}>
+                <Text style={{color: '#FFF', fontSize: '20px', textAlign: 'center'}}>Premium Quality</Text>
+                <Image style={{padding: '30px 30px'}} src='https://learners-deck-21-1143.sgp1.cdn.digitaloceanspaces.com/landingPage/hiker.png' />
+              </Col>
+              <Col span={6} style={{margin: '20px 40px'}}>
+                <Text style={{color: '#FFF', fontSize: '20px', textAlign: 'center'}}>Many Options</Text>
+                <Image style={{padding: '30px 30px'}} src='https://learners-deck-21-1143.sgp1.cdn.digitaloceanspaces.com/landingPage/hydra.png' />
+              </Col>
+              <Col span={6} style={{margin: '20px 40px'}}>
+                <Text style={{color: '#FFF', fontSize: '20px', textAlign: 'center'}}>Very Affordable</Text>
+                <Image style={{padding: '30px 30px'}} src='https://learners-deck-21-1143.sgp1.cdn.digitaloceanspaces.com/landingPage/fairy.png' />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row style={{ minHeight: '600px', background: 'url(https://learners-deck-21-1143.sgp1.cdn.digitaloceanspaces.com/landingPage/studentsIndia.webp)', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+
+        </Row>
+        <Row style={{ backgroundColor: '#FFF', minHeight: '600px' }}>
           <Col>
             <Text>Some text</Text>
           </Col>
--- src/client/pages/components/navbar/Navbar.js
+++ src/client/pages/components/navbar/Navbar.js
@@ -10,11 +10,11 @@ class Navbar extends Component {
     const {isAuthenticated} = this.props;
     return (
       <Fragment>
-        <nav style={{backgroundColor: '#0761D1', position: "sticky", top: "0", zIndex: "9999"}}>
+        <nav style={{backgroundColor: '#4C2889', position: "sticky", top: "0", zIndex: "9999"}}>
           <Grid.Container justify="center" alignItems="center" direction="row" >
             <Grid xs={6} md={4}>
               <div style={{display: 'flex', justifyContent: 'center'}}>
-              <Link to='/' style={{cursor: 'pointer', padding: '5px 5px', fontSize: '45px', color: '#FFF'}}>Logo</Link>
+              <Link to='/' style={{cursor: 'pointer', padding: '5px 5px', fontSize: '45px', color: '#FFF'}}>eBuzzet</Link>
               </div>
             </Grid>
             <Grid xs>
