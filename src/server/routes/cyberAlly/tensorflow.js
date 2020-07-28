af2bf0806738ff6d35fb9c31b184ba83c9d46352
Sahil Nare <sahilnare78@gmail.com>
2020-07-28 14:13:33 +0530

    Adde nsfwjs model

--- src/server/routes/cyberAlly/tensorflow.js
+++ src/server/routes/cyberAlly/tensorflow.js
@@ -1,17 +1,36 @@
 const router = require("express").Router();
 const tf = require("@tensorflow/tfjs-node");
 const tokenizers = require("tokenizers");
+const nsfw = require('nsfwjs');
+const axios = require('axios');
 // const spawn = require("child_process").spawn;
+
+let model1;
+
+tf.node.loadSavedModel("./saved_model").then((mdl) => {
+	model1 = mdl;
+});
+
+let wordPieceTokenizer;
+tokenizers.BertWordPieceTokenizer.fromOptions({ vocabFile:  "./vocab.txt", lowercase : false }).then(wPT => {
+	wordPieceTokenizer = wPT;
+	wordPieceTokenizer.setPadding({maxLength : 192});
+});
+
+let model_nsfw;
+nsfw.load().then(mdl => {
+	model_nsfw = mdl;
+});
 
 router.post('/model', async (req, res) => {
 	try {
 		const { sentence } = req.body;
 
-		const model = await tf.node.loadSavedModel("./saved_model");
-		console.log(model);
+		// const model = await tf.node.loadSavedModel("./saved_model");
+		// console.log(model);
 
-		const wordPieceTokenizer = await tokenizers.BertWordPieceTokenizer.fromOptions({ vocabFile: "./vocab.txt", lowercase : false });
-		wordPieceTokenizer.setPadding({maxLength : 192});
+		// const wordPieceTokenizer = await tokenizers.BertWordPieceTokenizer.fromOptions({ vocabFile: "./vocab.txt", lowercase : false });
+		// wordPieceTokenizer.setPadding({maxLength : 192});
 
 		const {ids} = await wordPieceTokenizer.encode(sentence);
 		console.log(ids);
@@ -20,7 +39,7 @@ router.post('/model', async (req, res) => {
 		    const input_tensor = tf.tensor(ids,undefined,"int32");
 		    const ip = input_tensor.expandDims(0);
 
-		    return model.predict({
+		    return model1.predict({
 		        "input_ids": ip
 		    });
 		});
@@ -33,6 +52,28 @@ router.post('/model', async (req, res) => {
 		res.json({answer: answer});
 
 
+
+	} catch (err) {
+			console.log(err);
+			res.sendStatus(500).json("Server error");
+	}
+});
+
+router.post('/nsfw', async (req, res) => {
+	try {
+
+		const { imgUrl } = req.body;
+
+		const pic = await axios.get(imgUrl, {
+	    responseType: 'arraybuffer',
+	  });
+	  // const model_nsfw = await nsfw.load();
+	  const image = await tf.node.decodeImage(pic.data,3);
+	  const predictions = await model_nsfw.classify(image);
+	  image.dispose();
+	  console.log(predictions);
+
+		res.json({predictions: predictions});
 
 	} catch (err) {
 			console.log(err);
