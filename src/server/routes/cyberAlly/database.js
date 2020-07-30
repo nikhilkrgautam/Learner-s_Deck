bc53dc57941e4691c24c0ce32465de9f42569ebf
Sahil Nare <sahilnare78@gmail.com>
2020-07-30 11:29:11 +0530

    Made seperate routes for each website

--- src/server/routes/cyberAlly/database.js
+++ src/server/routes/cyberAlly/database.js
@@ -16,4 +16,52 @@ router.get('/allcomments', async (req, res) => {
   }
 });
 
+router.get('/allcomments/youtube', async (req, res) => {
+  try {
+
+    const comments = await pool.query(
+      `SELECT website, comment, username, commentLink, user_id FROM comments WHERE website = $1`,
+      ['youtube']
+    );
+
+   res.json(comments.rows);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
+});
+
+router.get('/allcomments/twitter', async (req, res) => {
+  try {
+
+    const comments = await pool.query(
+      `SELECT website, comment, username, commentLink, user_id FROM comments WHERE website = $1`,
+      ['twitter']
+    );
+
+   res.json(comments.rows);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
+});
+
+router.get('/allcomments/facebook', async (req, res) => {
+  try {
+
+    const comments = await pool.query(
+      `SELECT website, comment, username, commentLink, user_id FROM comments WHERE website = $1`,
+      ['facebook']
+    );
+
+   res.json(comments.rows);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
+});
+
 module.exports = router;
